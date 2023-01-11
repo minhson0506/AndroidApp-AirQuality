1408f46a23112e4b35aa509fe2468eeb23b13ef2
Son Dang <minhson0506@gmail.com>
2023-01-11 15:45:31 +0200

    connect wifi automatically

--- app/src/main/java/com/example/airquality/MainActivity.kt
+++ app/src/main/java/com/example/airquality/MainActivity.kt
@@ -2,19 +2,19 @@ package com.example.airquality
 
 import android.Manifest
 import android.app.Activity
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.*
-import android.net.wifi.WifiManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.wifi.ScanResult
 import android.net.wifi.WifiNetworkSpecifier
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
@@ -25,8 +25,18 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ActivityCompat
 import com.example.airquality.ui.theme.AirQualityTheme
+import com.thanosfisherman.wifiutils.WifiUtils
+import com.thanosfisherman.wifiutils.wifiConnect.ConnectionErrorCode
+import com.thanosfisherman.wifiutils.wifiConnect.ConnectionSuccessListener
+import com.thanosfisherman.wifiutils.wifiScan.ScanResultsListener
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.net.SocketAddress
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.Executors.*
 
 
 class MainActivity : ComponentActivity() {
@@ -87,199 +97,52 @@ fun Greeting(name: String) {
 }
 
 fun connectWifi(context: Context) {
-    getWifi(context = context)
+//    getWifi(context = context)
+    Log.d("airquality", "connectWifi: start to try ")
+    connect(context, "ssid", "password")
+
 }
+
 
 fun disconnectWifi() {
 
 }
 
-fun getWifi(context: Context) {
+fun connect(context: Context, ssid:String, password: String) {
     val tag = "airquality"
 
-//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//        Log.d("TAG", "getWifi: start ")
-//        val mWifiManager =
-//            (context.getSystemService(Context.WIFI_SERVICE) as WifiManager)!!
-//        val info = mWifiManager.scanResults
-//        if (info != null)
-//        for ( elelment in info)
-//            Log.d("TAG", "getWifi: " + elelment.SSID)
-//        else
-//            Log.d("TAG", "getWifi: no wifi")
-//    } else {
-//        Log.d("TAG", "getWifi: android <= 10")
-//
-//    }
-
-    val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
-
-
-    fun connectWifi(context: Context, ssid: String?, password: String?) {
-        val mConnectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkSpecifier: NetworkSpecifier = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            WifiNetworkSpecifier.Builder()
-                .setSsid(ssid!!)
-                .setWpa2Passphrase(password!!)
-                .setIsHiddenSsid(true) //specify if the network does not broadcast itself and OS must perform a forced scan in order to connect
-                .build()
-        } else {
-            TODO("VERSION.SDK_INT < Q")
-        }
-        val networkRequest = NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .setNetworkSpecifier(networkSpecifier)
-            .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .build()
-        mConnectivityManager.requestNetwork(networkRequest, mNetworkCallback)
-    }
-    fun scanSuccess() {
-        val results = if (ActivityCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        } else {
-            wifiManager.scanResults
-        }
-        Log.d("TAG", "scanSuccess: ")
-        if (results != null) {
-            for (wifiNetwork in results) {
-                var split = wifiNetwork.toString().split(",")
-                Log.d("TAG", "scanSuccess: " + wifiNetwork.BSSID + split[0].split(":")[1].trim())
-                // connect to wifi
-                if (split[0].split(":")[1].trim() == "M&J") {
-                    Log.d("TAG", "scanSuccess: start to try connect wifi")
-
-//                    val networkSpecifier: NetworkSpecifier? =
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                            WifiNetworkSpecifier.Builder()
-//                                .setSsid(split[0].split(":")[1].trim())
-//                                .setWpa2Passphrase("12ba45678")
-//                                .setIsHiddenSsid(true) //specify if the network does not broadcast itself and OS must perform a forced scan in order to connect
-//                                .build()
-//                        } else {
-////                        TODO("VERSION.SDK_INT < Q")
-//                            null
-//                        }
-//                    val mNetworkCallback: NetworkCallback = object : NetworkCallback() {
-//                        override fun onAvailable(network: Network) {
-//                            super.onAvailable(network)
-//                            Log.d(tag,
-//                                "Wifi connected")
-//                        }
-//                    }
-//                    val networkRequest = NetworkRequest.Builder()
-//                        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-//                        .setNetworkSpecifier(networkSpecifier)
-//                        .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-//                        .build()
-//                    context.getSystemService(Context.CONNECTIVITY_SERVICE).requestNetwork(networkRequest, mNetworkCallback)
-
-//                    val suggestion = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                        WifiNetworkSuggestion.Builder()
-//                            .setSsid(split[0].split(":")[1].trim())
-//                            .setWpa2Passphrase("12ba45678")
-//                            .setIsAppInteractionRequired(true) // Optional (Needs location permission)
-//                            .build()
-//                    } else {
-//                        null
-//                    };
-//
-//                    val suggestionsList = listOf(suggestion);
-//                    if (suggestion != null) {
-//                        val status = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                            wifiManager.addNetworkSuggestions(suggestionsList)
-//                        } else {
-//                            null
-//                        };
-//                        if (status != WifiManager.STATUS_NETWORK_SUGGESTIONS_SUCCESS) {
-//                            // do error handling here
-//                        }
-//
-//// Optional (Wait for post connection broadcast to one of your suggestions)
-//                        val intentFilter = IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION);
-//
-//                        val broadcastReceiver = object : BroadcastReceiver() {
-//                            override fun onReceive(context: Context, intent: Intent) {
-//                                if (!intent.action.equals(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)) {
-//                                    return;
-//                                }
-//                                // do post connect processing here
-//                                Log.d("TAG", "onReceive: connected")
-//                            }
-//                        };
-//                        context.registerReceiver(broadcastReceiver, intentFilter);
-//                    }
-
-//                    var  connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE);
-//                    if (connectivityManager != null)
-//                    val networkCallback = object : ConnectivityManager.NetworkCallback() {
-//                        override fun onAvailable(network: Network) {
-//                            super.onAvailable(network)
-//                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                                // To make sure that requests don't go over mobile data
-//                                connectivityManager.bindProcessToNetwork(network)
-//                            } else {
-//                                connectivityManager.setProcessDefaultNetwork(network)
-//                            }
-//                        }
-//
-//                        override fun onLost(network: Network) {
-//                            super.onLost(network)
-//                            // This is to stop the looping request for OnePlus & Xiaomi models
-//                            connectivityManager.bindProcessToNetwork(null)
-//                            connectivityManager.unregisterNetworkCallback(networkCallback)
-//                            // Here you can have a fallback option to show a 'Please connect manually' page with an Intent to the Wifi settings
-//                        }
-//                    }
-
-                }
+    WifiUtils.withContext(context).enableWifi()
+    WifiUtils.withContext(context).connectWith(ssid, password).setTimeout(5000)
+        .onConnectionResult(object : ConnectionSuccessListener {
+            override fun success() {
+                Log.d(tag, "success connect wifi: ")
+                checkInternetConnection()
             }
-        } else {
-            Log.d("TAG", "scanSuccess: No wifi")
-        }
-//        ... use new scan results ...
-    }
 
-    fun scanFailure() {
-        // handle failure: new scan did NOT succeed
-        // consider using old scan results: these are the OLD results!
-        Log.d("TAG", "scanFailure: ")
-        val results = wifiManager.scanResults
-//        ... potentially use older scan results ...
-    }
-
-    val wifiScanReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
-            if (success) {
-                scanSuccess()
-            } else {
-                scanFailure()
+            override fun failed(errorCode: ConnectionErrorCode) {
+                Log.d(tag, "connect failed: $errorCode")
             }
+        }).start()
+
+}
+
+private fun checkInternetConnection() {
+    val tag = "airquality"
+    Log.d(tag, "checkInternetConnection: ")
+    val executorService: ExecutorService = newFixedThreadPool(4)
+    executorService.execute {
+        try {
+            val timeoutMs = 1500
+            val sock = Socket()
+            val sockaddr: SocketAddress = InetSocketAddress("8.8.8.8", 53)
+            sock.connect(sockaddr, timeoutMs)
+            sock.close()
+            Log.d(tag, "checkInternetConnection: success")
+
+        } catch (e: Exception) {
+            Log.d(tag, "checkInternetConnection: fail")
         }
     }
-
-    val intentFilter = IntentFilter()
-    intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-    context.registerReceiver(wifiScanReceiver, intentFilter)
-
-    // start to scan wifi network
-    val success = wifiManager.startScan()
-    if (!success) {
-        // scan failure handling
-        scanFailure()
-    }
-
-
 }
 
 @Preview(showBackground = true)
