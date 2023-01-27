4954c9f896ebd0ce116040a1752ed105c7eafa9b
Son Dang <minhson0506@gmail.com>
2023-01-27 13:33:18 +0200

    imlement retrofit with wifi

--- app/src/main/java/com/example/airquality/MainActivity.kt
+++ app/src/main/java/com/example/airquality/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.airquality
 import android.Manifest
 import android.app.Activity
 import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.net.*
 import android.net.ConnectivityManager.NetworkCallback
@@ -30,6 +31,15 @@ import com.thanosfisherman.wifiutils.WifiUtils
 import com.thanosfisherman.wifiutils.wifiConnect.ConnectionErrorCode
 import com.thanosfisherman.wifiutils.wifiConnect.ConnectionSuccessListener
 import com.thanosfisherman.wifiutils.wifiScan.ScanResultsListener
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Query
 import java.net.InetAddress
 import java.net.InetSocketAddress
 import java.net.Socket
@@ -83,6 +93,8 @@ fun Greeting(name: String) {
     val context = LocalContext.current
     var wifiName by remember { mutableStateOf("") }
 
+    testWeatherApi(context = context)
+
     Column() {
         Button(onClick = { connectWifi(context) }) {
             Text(text = "Start Connect Wifi")
@@ -97,11 +109,53 @@ fun Greeting(name: String) {
 //    Text(text = "Hello $name!"
 }
 
+fun testWeatherApi(context: Context) {
+    checkInternetConnection()
+    Service.instance().sendReq().enqueue(object : Callback<ResponseModel> {
+        override fun onResponse(call: Call<ResponseModel>, response: Response<ResponseModel>) {
+            Toast.makeText(context, response.message().toString(), Toast.LENGTH_LONG).show()
+            Log.d("airquality", "onResponse: " + response.message().toString())
+        }
+
+        override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+            Toast.makeText(context, t.toString(), Toast.LENGTH_LONG).show()
+                Log.d("airquality", "onFailure: " + t.toString())
+        }
+    })
+
+}
+
+data class ResponseModel(val current: String, val location: String) {
+    override fun toString(): String {
+        return super.toString()
+    }
+}
+
+data class RequestModel(val key: String = "ed7111cc88ee4769858141158222207")
+
+interface Api {
+    @GET("current.json")
+    fun sendReq(
+        @Query("key") key: String = "ed7111cc88ee4769858141158222207",
+        @Query("q") location: String = "Hanoi&days=10&aqi=yes&alerts=yes",
+    ): Call<ResponseModel>
+}
+
+object Service {
+
+    fun instance(): Api {
+        return Retrofit.Builder().baseUrl("https://api.weatherapi.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(Api::class.java)
+    }
+
+}
+
 fun connectWifi(context: Context) {
 //    getWifi(context = context)
     Log.d("airquality", "connectWifi: start to try ")
 //    connect(context, "ssid", "password")
-scan(context)
+    scan(context)
 }
 
 
@@ -125,15 +179,15 @@ fun getScanResults(result: List<ScanResult>) {
     }
     Log.d(tag, "getScanResults: " + result.size)
 
-    Log.d(tag, "getScanResults: " + result )
+    Log.d(tag, "getScanResults: " + result)
 
-    Log.d(tag, "getScanResults first item: " + result[0] )
+    Log.d(tag, "getScanResults first item: " + result[0])
 
-    Log.d(tag, "getScanResults first item: " + result[0].toString().split(",")[0] )
+    Log.d(tag, "getScanResults first item: " + result[0].toString().split(",")[0])
 
 }
 
-fun connect(context: Context, ssid:String, password: String) {
+fun connect(context: Context, ssid: String, password: String) {
     val tag = "airquality"
 
     WifiUtils.withContext(context).enableWifi()
